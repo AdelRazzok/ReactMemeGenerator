d350abdeb360c4b7d4fb2e913d1aa7a53a35dea8
= <LHP6.arazzok@gmail.com>
2022-03-31 17:51:29 +0200

    add image meme and handling click on the btn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Header from './components/Header'
-import Form from './components/Form'
+import Meme from './components/Meme'
 
 export default function App() {
 	return (
 		<div className="App">
 			<Header />
-			<Form />
+			<Meme />
 		</div>
 	)
 }
---
+++ src/components/Meme.js
@@ -0,0 +1,27 @@
+import {useState} from 'react'
+import memesData from '../memesData'
+
+export default function Meme() {
+	const getMemeImg = () => {
+		const memesArray = memesData.data.memes
+		const randomInt = Math.floor(Math.random() * memesArray.length)
+		return memesArray[randomInt].url
+	}
+
+	const [url, setUrl] = useState(getMemeImg)
+	
+	const handleClick = () => setUrl(getMemeImg)
+	
+	return (
+		<main className='Meme'>
+			<div className='Form'>
+				<input type="text" placeholder='Your top text here...' className='Form-input' />
+				<input type="text" placeholder='Your bottom text here...' className='Form-input' />
+				<button type='button' onClick={handleClick} className='Form-button'>
+					Get a new meme image 🖼️
+				</button>
+			</div>
+			<img src={url} alt='Meme image' className='Meme-image' />
+		</main>
+	)
+}
