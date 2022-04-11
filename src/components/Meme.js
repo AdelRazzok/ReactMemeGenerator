1a6d02c3fe95151d0f80aaa3feb0b010724afa00
= <LHP6.arazzok@gmail.com>
2022-04-11 11:27:01 +0200

    add a State in order to keep inputs value on every keystroke

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,6 +2,18 @@ import {useState} from 'react'
 import memesData from '../memesData'
 
 export default function Meme() {
+	const [formData, setFormData] = useState(
+		{firstName: '', lastName: ''}
+	)
+	const handleChange = (event) => {
+		setFormData(prev => {
+			return {
+				...prev,
+				[event.target.name]: event.target.value
+			}
+		})
+	}
+
 	const getMemeImg = () => {
 		const memesArray = memesData.data.memes
 		const randomInt = Math.floor(Math.random() * memesArray.length)
@@ -20,9 +32,25 @@ export default function Meme() {
 	return (
 		<main className='Meme'>
 			<div className='Form'>
-				<input type="text" placeholder='Your top text here...' className='Form-input' />
-				<input type="text" placeholder='Your bottom text here...' className='Form-input' />
-				<button type='button' onClick={handleClick} className='Form-button'>
+				<input
+					type='text'
+					placeholder='Your top text here...'
+					className='Form-input'
+					name='firstName'
+					onChange={handleChange}
+				/>
+				<input
+					type='text'
+					placeholder='Your bottom text here...'
+					className='Form-input'
+					name='lastName'
+					onChange={handleChange}
+				/>
+				<button
+					type='button'
+					onClick={handleClick}
+					className='Form-button'
+				>
 					Get a new meme image 🖼️
 				</button>
 			</div>
